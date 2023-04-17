98592ec07867bf3e9dfcc564f4cb61d1c23d66ad
Kadaj99 <65830119+Kadaj99@users.noreply.github.com>
2023-04-18 01:47:24 +0200

    Add files via upload

--- Plane/src/sdl2/main.cpp
+++ Plane/src/sdl2/main.cpp
@@ -11,3 +11,14 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
+
+/*
+||=== Build: Debug in Plane (compiler: GNU GCC Compiler) ===|
+/home/kadaj/Doc/Plane/src/sdl2/mainSDL.cpp||In member function ‘void Game::run()’:|
+/home/kadaj/Doc/Plane/src/sdl2/mainSDL.cpp|231|warning: enumeration value ‘ControlMenu’ not handled in switch [-Wswitch]|
+/home/kadaj/Doc/Plane/src/sdl2/mainSDL.cpp|231|warning: enumeration value ‘Paused’ not handled in switch [-Wswitch]|
+/home/kadaj/Doc/Plane/src/sdl2/mainSDL.cpp||In member function ‘void Game::handleRunningEvents(SDL_Event&)’:|
+/home/kadaj/Doc/Plane/src/sdl2/mainSDL.cpp|302|warning: comparison of integer expressions of different signedness: ‘Uint32’ {aka ‘unsigned int’} and ‘const int’ [-Wsign-compare]|
+||=== Build finished: 0 error(s), 3 warning(s) (0 minute(s), 1 second(s)) ===|
+||=== Run: Debug in Plane (compiler: GNU GCC Compiler) ===|
+*/
